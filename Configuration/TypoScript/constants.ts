31894c5719b583404645e7ede396e5ac0cb7437b
Alexander Bigga <alexander.bigga@slub-dresden.de>
2018-10-30 17:13:54 +0100

    rename typoscript path of plugin

--- Configuration/TypoScript/constants.ts
+++ Configuration/TypoScript/constants.ts
@@ -1,15 +1,14 @@
-
-plugin.tx_slubentityfacts_entityfactslisting {
+plugin.tx_slubentityfacts {
   view {
-    # cat=plugin.tx_slubentityfacts_entityfactslisting/file; type=string; label=Path to template root (FE)
+    # cat=plugin.tx_slubentityfacts/file; type=string; label=Path to template root (FE)
     templateRootPath = EXT:slub_entityfacts/Resources/Private/Templates/
-    # cat=plugin.tx_slubentityfacts_entityfactslisting/file; type=string; label=Path to template partials (FE)
+    # cat=plugin.tx_slubentityfacts/file; type=string; label=Path to template partials (FE)
     partialRootPath = EXT:slub_entityfacts/Resources/Private/Partials/
-    # cat=plugin.tx_slubentityfacts_entityfactslisting/file; type=string; label=Path to template layouts (FE)
+    # cat=plugin.tx_slubentityfacts/file; type=string; label=Path to template layouts (FE)
     layoutRootPath = EXT:slub_entityfacts/Resources/Private/Layouts/
   }
   persistence {
-    # cat=plugin.tx_slubentityfacts_entityfactslisting//a; type=string; label=Default storage PID
+    # cat=plugin.tx_slubentityfacts/200/a; type=int+; label=Default storage PID
     storagePid =
   }
 }
