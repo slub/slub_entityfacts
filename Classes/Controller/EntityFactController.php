b8cf92f789b862de0a44f390168fc8428b33cdb8
Alexander Bigga <alexander.bigga@slub-dresden.de>
2018-10-30 18:40:23 +0100

    feature: show all if no item is selected inside multiArraySelection

--- Classes/Controller/EntityFactController.php
+++ Classes/Controller/EntityFactController.php
@@ -17,7 +17,7 @@ namespace Slub\SlubEntityfacts\Controller;
  */
 class EntityFactController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
 {
-    
+
     /**
      * action show
      *
@@ -26,61 +26,63 @@ class EntityFactController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionContr
     public function showAction()
     {
         //$entityFacts = $this->entityFactRepository->findAll();
-                
+
         //Get the nine chars long entity facts id from Flexform that the user wants to call
-        $search = $this->settings['entityfacts']['personality'];    
-        
+        $search = $this->settings['entityfacts']['personality'];
+
         //Get the selected entity type from Flexform
-        $selection = $this->settings['entityfacts']['selection'];       
-        
+        $selection = $this->settings['entityfacts']['selection'];
+
         $arguments = $this->request->getArguments();
         //print_r($arguments);
-        
+
         if ($arguments['search']){
             $search = $arguments['search'];
-        } 
-        
+        }
+
         //Call the API and write the decoded JSON to the array
 		$apiAnswer = file_get_contents('http://hub.culturegraph.org/entityfacts/'.$search);
-		
+
 		//Replace @id for easier calling of informations (no value with "@id" expected)
 		$apiAnswerClean = str_replace('"@id"', '"atid"', $apiAnswer);
-		
+
 		$apiAnswerDecode = json_decode ($apiAnswerClean, true);
-		
-		
+
+
 		//Workaround to get rid of the'@'
-		$apiAnswerDecode['context'] = $apiAnswerDecode['@context'];       
+		$apiAnswerDecode['context'] = $apiAnswerDecode['@context'];
 		//$apiAnswerDecode['id'] = $apiAnswerDecode['@id'];
 		$apiAnswerDecode['type'] = $apiAnswerDecode['@type'];
-		
-		
+
+
 		//write the user sorted selection given by Flexform into helper array
-        $multiSelectArray1 = explode(",",($this->settings['entityfacts'][$selection.'facts'])); 
-        
-        //write the user sorted selection given by Flexform into another helper array (special customer request to select specific entities from sameAs)
-        $multiSelectArray2 = explode(",",($this->settings['entityfacts']['sameAsSelected']));       
-        
-        $sameAsArray = [];
-        
-        //Fill new array with original infos to have an easier use in Fluid
-        foreach ($multiSelectArray1 as $item) {                         
-            $viewArray[$item] = $apiAnswerDecode[$item];
+        $multiSelectArray1 = explode(",",($this->settings['entityfacts'][$selection.'facts']));
+
+        if ($this->settings['entityfacts'][$selection.'facts']) {
+            //Fill new array with original infos to have an easier use in Fluid
+            foreach ($multiSelectArray1 as $item) {
+                $viewArray[$item] = $apiAnswerDecode[$item];
+            }
+        } else {
+            // no fact is selected --> show all
+            $viewArray = $apiAnswerDecode;
         }
-        
-        
+
+        //write the user sorted selection given by Flexform into another helper array (special customer request to select specific entities from sameAs)
+        $multiSelectArray2 = explode(",",($this->settings['entityfacts']['sameAsSelected']));
+
+        $sameAsArray = [];
+
         //Same function like the first array but with additional customisation
-        foreach ($multiSelectArray2 as $item) {  
+        foreach ($multiSelectArray2 as $item) {
             foreach ($apiAnswerDecode['sameAs'] as $sameAs) {
-            
-                if ($sameAs['collection']['abbr'] == $item) {
+                if ($sameAs['collection']['abbr'] == $item || empty($this->settings['entityfacts']['sameAsSelected'])) {
                     $sameAsArray[ $sameAs['collection']['abbr'] ] = $sameAs;
                 }
             }
-            
         }
-        
-		
+
+
 		$this->view->assign('sameAsArray', $sameAsArray);
 		$this->view->assign('viewArray', $viewArray);
 		//$this->view->assign('multiSelectArray', $multiSelectArray);
