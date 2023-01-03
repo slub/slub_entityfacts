4870f0e0d5b87f655197a9fac36a94ee7df8db3c
Michael Grundkötter <mg@ujamii.com>
2023-01-03 15:18:32 +0100

    [TASK] added compat for v10 and v11

--- ext_emconf.php
+++ ext_emconf.php
@@ -24,7 +24,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.2.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '7.6.0-9.5.99',
+            'typo3' => '10.4.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
